e3d826f4b65bb8811feb10c3cf46c4694fad3fd2
josueramirez1 <josue67ramirez@gmail.com>
2024-06-17 17:52:27 -0700

    removing javascript form in script/adding onsubmit

--- script.js
+++ script.js
@@ -52,14 +52,3 @@ navBtn.addEventListener("click", (e) => {
     header.classList.remove("nav-open");
   });
 });
-
-// Form submission
-const form = document.querySelector(".cta-form");
-const submitBtn = document.querySelector(".btn--form");
-
-form.addEventListener("submit", (e) => {
-  for (let div of form) {
-    div.value = "";
-  }
-  submitBtn.textContent = "Your form has been received. Thank you!";
-});
